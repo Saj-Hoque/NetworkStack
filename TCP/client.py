70371c0005f443b9ed4d411ea3a9fe586c01c2b3
Saj-Hoque <sajuss01@gmail.com>
2023-12-05 12:07:20 +0000

    Create TCP Folder

---
+++ TCP/client.py
@@ -0,0 +1,93 @@
+# SYS4 lab assessment - part 1
+# Ben Howard
+# created: 07/11/2023 | Current: 14/11/2023
+
+# !!!!Use on linux (use vm to test on laptop)!!!! - Check options on that
+# TODO: How to ensure only server reads request and client reponce?
+# TODO: File generation
+# TODO: Implement check that responce is as expected?
+
+import os
+
+SERVER_PIPE = "server"
+CLIENT_PIPE = "client"
+
+class Client():
+    def __init__(self, clientPath, serverPath):
+        self._ClientPipe = clientPath
+        self._ServerPipe = serverPath
+
+    # main
+    def open_connection(self):
+
+        # create pipe if it does not already exist
+        if not os.path.exists(self._ClientPipe):
+            os.mkfifo(self._ClientPipe)
+    
+    def send_request(self, rMethord, rURI='/', rHost='www.example.com'): #None is temp
+    # '''
+    # Will generate and send a request 
+    # ================================
+    # args:
+    #     rMethord :: String # Expects 'head' or 'get' otherwise throws ValueError
+    #     rURI :: String # URI for request - defaults to / (potential problem with * in current setup?)
+    #     rHost :: String
+    
+    # rtns
+    #     str # responce from server
+
+    # throws:
+    #     ValueError # If methord is not suported
+    # '''
+        requestPipe = open(self._ServerPipe, 'w')
+        # opens pipe
+        
+        if not rMethord.upper() in ['GET', 'HEAD']:
+            raise ValueError
+        
+        request = f'{rMethord.upper()} {rURI} HTTP/1.1\nHost:{rHost}'
+        
+        print(request)
+
+        # Only allows for simple requests currently (Host only)
+        # Will expand if more methords are needed
+        
+
+
+        self._generate_file(request)
+        
+        requestPipe.write(request)
+        requestPipe.close()
+        
+    
+
+    # extra
+    def _check_pipe(self):
+    # '''
+    # Checks the server is running and has opened a pipe
+
+    # throws:
+    #     Exception # If server has not opened pipe
+    # '''
+        if not os.path.exists(self._ServerPipe):
+            # Need to discuss OS compatibility for pipes
+            raise Exception("Error: server not open")
+        
+    def _generate_file(self, content):
+        # TODO
+        pass
+
+
+
+client = Client(CLIENT_PIPE, SERVER_PIPE)
+client._check_pipe()
+client.open_connection()
+req = input("")
+client.send_request(req)
+with open(CLIENT_PIPE, 'r') as response_pipe:
+    # While the server connection is stable read any incoming requests from the named pipe
+    request = response_pipe.read()
+
+print(request)
+
+os.remove(CLIENT_PIPE)
